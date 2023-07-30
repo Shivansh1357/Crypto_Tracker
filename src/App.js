6ba3dfd716d457c0326ec1a7041d368e01b0fd39
Shivansh1357 <120310888+Shivansh1357@users.noreply.github.com>
2023-07-30 11:32:40 +0530

    Pointer Removed

--- src/App.js
+++ src/App.js
@@ -14,44 +14,44 @@ import "react-toastify/dist/ReactToastify.css";
 
 
 function App() {
-  var cursor;
-  var cursorPointer;
-  useEffect(() => {
-    cursor = document.getElementById("cursor");
-    cursorPointer = document.getElementById("cursor-pointer");
+  // var cursor;
+  // var cursorPointer;
+  // useEffect(() => {
+  //   cursor = document.getElementById("cursor");
+  //   cursorPointer = document.getElementById("cursor-pointer");
 
-    document.body.addEventListener("mousemove", function (e) {
-      return (
-        (cursor.style.left = e.clientX + "px"),
-        (cursor.style.top = e.clientY + "px"),
-        (cursorPointer.style.left = e.clientX + "px"),
-        (cursorPointer.style.top = e.clientY + "px")
-      );
-    });
+  //   document.body.addEventListener("mousemove", function (e) {
+  //     return (
+  //       (cursor.style.left = e.clientX + "px"),
+  //       (cursor.style.top = e.clientY + "px"),
+  //       (cursorPointer.style.left = e.clientX + "px"),
+  //       (cursorPointer.style.top = e.clientY + "px")
+  //     );
+  //   });
 
-    document.body.addEventListener("mousedown", function (e) {
-      return (
-        (cursor.style.height = "0.5rem"),
-        (cursor.style.width = "0.5rem"),
-        (cursorPointer.style.height = "3rem"),
-        (cursorPointer.style.width = "3rem")
-      );
-    });
+  //   document.body.addEventListener("mousedown", function (e) {
+  //     return (
+  //       (cursor.style.height = "0.5rem"),
+  //       (cursor.style.width = "0.5rem"),
+  //       (cursorPointer.style.height = "3rem"),
+  //       (cursorPointer.style.width = "3rem")
+  //     );
+  //   });
 
-    document.body.addEventListener("mouseup", function (e) {
-      return (
-        (cursor.style.height = "0.3rem"),
-        (cursor.style.width = "0.3rem"),
-        (cursorPointer.style.height = "2rem"),
-        (cursorPointer.style.width = "2rem")
-      );
-    });
-  }, []);
+  //   document.body.addEventListener("mouseup", function (e) {
+  //     return (
+  //       (cursor.style.height = "0.3rem"),
+  //       (cursor.style.width = "0.3rem"),
+  //       (cursorPointer.style.height = "2rem"),
+  //       (cursorPointer.style.width = "2rem")
+  //     );
+  //   });
+  // }, []);
 
   return (
     <div className="App">
-    <div className="cursor" id="cursor" />
-    <div className="cursor-pointer" id="cursor-pointer" />
+    {/* <div className="cursor" id="cursor" />
+    <div className="cursor-pointer" id="cursor-pointer" /> */}
     <ToastContainer/>
       <Routes>
         <Route path='/' element={<HomePage/>}/>
